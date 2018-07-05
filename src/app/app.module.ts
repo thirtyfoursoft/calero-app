ebfda99af995ab324a4578926bc850602d903460
sanjit <sanjit>
2018-07-05 18:22:54 +0530

    Adding Navigation componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SubnavComponent } from './subnav/subnav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    SubnavComponent
   ],
   imports: [
     BrowserModule
---
+++ src/custom.js
@@ -0,0 +1,5 @@
+//Offside.js minimal setup
+var myOffside = offside( '#my-menu', {
+	slidingElementsSelector:'#my-content-container',
+	buttonsSelector: '#my-button, .another-button',
+});
