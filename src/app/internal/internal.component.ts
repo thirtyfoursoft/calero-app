d720e9710e1c2c75c648971e2769b13c5b397213
thirtyfour <info@thirtyfour.in>
2018-07-16 18:48:06 +0530

    Adding page title

--- src/app/internal/internal.component.ts
+++ src/app/internal/internal.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./internal.component.css']
 })
 export class InternalComponent implements OnInit {
-
+	page_title ="Internal team";
   constructor() { }
 
   ngOnInit() {
